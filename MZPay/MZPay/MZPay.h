87ae02548525605f06ea3a22edf4cf41b494c09c
1691665955 <1691665955@qq.com>
2022-07-20 11:15:16 +0800

    update

---
+++ MZPay/MZPay/MZPay.h
@@ -0,0 +1,18 @@
+//
+//  MZPay.h
+//  MZPay
+//
+//  Created by 曾龙 on 2022/7/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MZPay.
+FOUNDATION_EXPORT double MZPayVersionNumber;
+
+//! Project version string for MZPay.
+FOUNDATION_EXPORT const unsigned char MZPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MZPay/PublicHeader.h>
+
+
---
+++ MZPay/MZPay/mapoc/myheader.h
@@ -0,0 +1,14 @@
+//
+//  myheader.h
+//  MZPay
+//
+//  Created by 曾龙 on 2022/7/19.
+//
+
+#ifndef myheader_h
+#define myheader_h
+
+#import <WechatOpenSDK/WXApi.h>
+#import <AlipaySDK/AlipaySDK.h>
+
+#endif /* myheader_h */
